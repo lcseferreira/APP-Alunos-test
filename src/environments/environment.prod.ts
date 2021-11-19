1b45e66b19d75a72f05d21cb34610d74cb3d576c
Lucas Ferreira <lcseferreira@hotmail.com>
2021-11-18 21:11:45 -0300

    _redirects oh god

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiURLBase: 'https://alunos-app-test.herokuapp.com/',
+  apiURLBase: 'https://alunos-app-test.herokuapp.com',
   clientID: 'my-angular-app',
   clientSecret: 'my-pass-app',
   getUrlToken: '/oauth/token',
