48fc2eda636cdd517432f34cc346d6ea05ae60ea
Lucas Ferreira <lcseferreira@hotmail.com>
2021-11-18 21:13:17 -0300

    test env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: false,
+  production: true,
   apiURLBase: 'https://alunos-app-test.herokuapp.com',
   clientID: 'my-angular-app',
   clientSecret: 'my-pass-app',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   apiURLBase: 'https://alunos-app-test.herokuapp.com',
   clientID: 'my-angular-app',
   clientSecret: 'my-pass-app',
